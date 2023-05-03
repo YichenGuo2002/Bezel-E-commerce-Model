a0bc5fc4a6c73b3976be465b66321ee5b52f1fb3
Yichen Guo <yg2258@nyu.edu>
2023-05-03 09:07:51 -0400

    styled action panel

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import './App.css';
-import './style.css';
+import './App.css'; // page css
+import './style.css'; // tailwind css script
 
 function App() {
   return (
-    <div className="App">
-      <div className = "actionPanel">
-        <h3>CONGRATS!</h3>
-        <h1>Your watch sold!</h1>
-        <h5>You have 1 business day to accept the sale. 
-          If you do not accept, it will be automatically rejected.
-        </h5>
-        <button>Accept sale</button>
-        <button>Reject sale</button>
-      </div>
+    <div className = "fixed top-0 left-0 w-full h-full flex items-center justify-center font-sans">
+      <div className="popUp bg-popup w-3/5 h-4/6 grid grid-cols-2 rounded-3xl p-10">
+        
+        <div className = "actionPanel h-full flex flex-col place-content-between pl-4 pr-4">
+          <div className = "p-8">
+            <p className = "text-base mb-2 text-neutral-400">CONGRATS!</p>
+            <p className = "text-2xl mb-8 text-btn font-bold">Your watch sold!</p>
+            <p className = "text-base text-btn-sm">You have 1 business day to accept the sale. 
+              If you do not accept, it will be automatically rejected.
+            </p>
+          </div>
+          <div className = "">
+            <button className = "w-full bg-btn text-popup rounded-3xl p-4">Accept sale</button>
+            <button className = "w-full text-btn rounded-3xl p-4">Reject sale</button>
+          </div>
+        </div>
 
-      <div className = "productPanel">
-      <hr/>
-      <h3>Selling Price</h3>
-      <h3>Level 1 Commission (6.5%)</h3>
-      <h3>Seller fee</h3>
-      <h3>Insured Shipping</h3>
-      <h3>Bezel authentication</h3>
-      <hr/>
-      <h3>Earnings</h3>
-      </div>
+        <div className = "productPanel h-full border bg-watch rounded-3xl p-10">
+          <hr className = "border border-neutral-300"/>
+          <h3>Selling Price</h3>
+          <h3>Level 1 Commission (6.5%)</h3>
+          <h3>Seller fee</h3>
+          <h3>Insured Shipping</h3>
+          <h3>Bezel authentication</h3>
+          <hr className = "border border-neutral-300"/>
+          <h3>Earnings</h3>
+        </div>
 
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Source Sans Pro', 'sans-serif'],
+    },
   },
   plugins: [],
 }
