9c0862b92e0dc73fabe98ef8a997739cfe0e714c
Yichen Guo <yg2258@nyu.edu>
2023-05-03 07:49:01 -0400

    page set up, tailwind css set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "actionPanel">
+        <h3>CONGRATS!</h3>
+        <h1>Your watch sold!</h1>
+        <h5>You have 1 business day to accept the sale. 
+          If you do not accept, it will be automatically rejected.
+        </h5>
+        <button>Accept sale</button>
+        <button>Reject sale</button>
+      </div>
+
+      <div className = "productPanel">
+      <hr/>
+      <h3>Selling Price</h3>
+      <h3>Level 1 Commission (6.5%)</h3>
+      <h3>Seller fee</h3>
+      <h3>Insured Shipping</h3>
+      <h3>Bezel authentication</h3>
+      <hr/>
+      <h3>Earnings</h3>
+      </div>
+
     </div>
   );
 }
